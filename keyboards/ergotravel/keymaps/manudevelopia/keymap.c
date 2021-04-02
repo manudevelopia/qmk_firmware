1d0c680143c23cb0ddb44097e4209baadbf3a598
manudevelopia <manu@developia.info>
2021-04-02 16:41:20 +0200

    set last adjustmets

--- keyboards/ergotravel/keymaps/manudevelopia/keymap.c
+++ keyboards/ergotravel/keymaps/manudevelopia/keymap.c
@@ -31,31 +31,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,          KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_LGUI, KC_LCTL, KC_LALT, KC_LGUI,           LOWER,  KC_SPC,           KC_SPC,  RAISE,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+     KC_LGUI, KC_LCTL, KC_LALT, KC_LGUI,           LOWER,  KC_SPC,           KC_ENT,  RAISE,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
   ),
 
   [_LOWER] = LAYOUT(
-     KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,         KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN,  KC_RPRN, KC_DEL,  
-     KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LCBR,         KC_RCBR, KC_LEFT, KC_UP,   KC_DOWN,  KC_RGHT,  KC_BSLS, KC_PIPE,
-     KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC,         ALFRED,  KC_UNDO, KC_CUT,  KC_COPY,  KC_PASTE, KC_FIND, _______,
-     _______, _______,  _______, _______, _______, KC_BSPC,                           ALFRED,  ADJUST,  KC_HOME,  KC_PGUP,  KC_PGDN, KC_END 
+     KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,         KC_PLUS, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  
+     KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LCBR,         KC_RCBR, KC_LEFT, KC_UP,    KC_DOWN, KC_RGHT, KC_BSLS, KC_PIPE,
+     _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_BSPC,         ALFRED,  KC_COPY, KC_PASTE, _______, _______, _______, _______,
+     _______, _______,  _______, _______, _______, KC_BSPC,                           ALFRED,  ADJUST,   KC_HOME, KC_PGUP, KC_PGDN, KC_END 
   ),
  
   [_RAISE] = LAYOUT(
-     KC_TILD, KC_1,     KC_2,    KC_3,    KC_PLUS, KC_MINS,  _______,        _______, KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
+     KC_TILD, KC_1,     KC_2,    KC_3,    KC_PLUS, KC_MINS,  _______,        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
      _______, KC_4,     KC_5,    KC_6,    KC_PAST, KC_PSLS,  _______,        _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
      _______, KC_7,     KC_8,    KC_9,    KC_ENT,  _______,  ALFRED,         _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,
      _______, KC_COMM,  KC_0,    KC_DOT,  _______, ALFRED,                            KC_BSPC, _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT 
   ),
 
   [_ADJUST] = LAYOUT(
-     _______, _______,  _______, _______, _______, _______, _______,         _______,  _______, RGB_MOD, RGB_VAI, RGB_SAI, RGB_HUI, KC_PWR,
-     _______, _______,  _______, _______, _______, _______, _______,         _______,  _______, RGB_RMOD, RGB_VAD, RGB_SAD, RGB_HUD, RGB_TOG,
-     _______, _______,  _______, _______, _______, _______, _______,         _______,  _______, _______, _______, _______, _______, BL_STEP,
-     _______, _______, _______, _______,  _______, _______,                            _______, _______, _______, _______, _______, RESET
+     _______, _______, _______, _______, _______, _______, _______,         _______,  _______, RGB_MOD,  RGB_VAI, RGB_SAI, RGB_HUI, KC_PWR,
+     _______, _______, _______, _______, _______, _______, _______,         _______,  _______, RGB_RMOD, RGB_VAD, RGB_SAD, RGB_HUD, RGB_TOG,
+     _______, _______, _______, _______, _______, _______, _______,         _______,  _______, _______,  _______, _______, _______, BL_STEP,
+     _______, _______, _______, _______, _______, _______,                            _______, _______,  _______, _______, _______, RESET
   )
 
 };
