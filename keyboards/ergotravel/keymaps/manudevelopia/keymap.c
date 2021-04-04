d6d9e5b8a74ddd9a3d9e14d06d0adeb0c44d5d73
manudevelopia <manu@developia.info>
2021-04-04 22:53:20 +0200

    add grave to keyboard

--- keyboards/ergotravel/keymaps/manudevelopia/keymap.c
+++ keyboards/ergotravel/keymaps/manudevelopia/keymap.c
@@ -36,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LGUI, KC_LCTL, KC_LALT, KC_LGUI,           LOWER,  KC_SPC,           KC_ENT,  RAISE,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
   ),
-
+```~~~
   [_LOWER] = LAYOUT(
-     KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,         KC_PLUS, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  
+     KC_GRV,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,         KC_PLUS, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  
      KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LCBR,         KC_RCBR, KC_LEFT, KC_UP,    KC_DOWN, KC_RGHT, KC_BSLS, KC_PIPE,
      _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_BSPC,         ALFRED,  KC_COPY, KC_PASTE, _______, _______, _______, _______,
      _______, _______,  _______, _______, _______, KC_BSPC,                           ALFRED,  ADJUST,   KC_HOME, KC_PGUP, KC_PGDN, KC_END 
