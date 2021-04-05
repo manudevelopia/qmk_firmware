96aee7e92d7ed466039f57f7f22b2889b7e9bee0
manudevelopia <manu@developia.info>
2021-04-05 11:45:32 +0200

    move coma and dot

--- keyboards/ergotravel/keymaps/manudevelopia/keymap.c
+++ keyboards/ergotravel/keymaps/manudevelopia/keymap.c
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
  
   [_RAISE] = LAYOUT(
-     KC_TILD, KC_1,     KC_2,    KC_3,    KC_PLUS, KC_MINS,  _______,        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
-     _______, KC_4,     KC_5,    KC_6,    KC_PAST, KC_PSLS,  _______,        _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-     _______, KC_7,     KC_8,    KC_9,    KC_ENT,  _______,  ALFRED,         _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,
-     _______, KC_COMM,  KC_0,    KC_DOT,  _______, ALFRED,                            KC_BSPC, _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT 
+     KC_TILD, KC_1,     KC_2,    KC_3,     KC_PLUS, KC_MINS,  _______,        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
+     _______, KC_4,     KC_5,    KC_6,     KC_PAST, KC_PSLS,  _______,        _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+     _______, KC_7,     KC_8,    KC_9,     KC_COMM, KC_DOT,  ALFRED,         _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,
+     _______, _______,  KC_0,    _______,  _______, ALFRED,                            KC_BSPC, _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT 
   ),
 
   [_ADJUST] = LAYOUT(
