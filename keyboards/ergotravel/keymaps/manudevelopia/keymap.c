0ccf3b6d9612441b42e4c4ed253e914d1c1f8bb8
manudevelopia <manu@developia.info>
2021-04-16 23:21:35 +0200

    fix cursors

--- keyboards/ergotravel/keymaps/manudevelopia/keymap.c
+++ keyboards/ergotravel/keymaps/manudevelopia/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
@@ -33,22 +32,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_LGUI, KC_LCTL, KC_LALT, KC_LGUI,           LOWER,  KC_SPC,           KC_ENT,  RAISE,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+     KC_LGUI, KC_LCTL, KC_LALT, KC_LGUI,           LOWER,  KC_SPC,           KC_ENT,  RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
   ),
 
   [_LOWER] = LAYOUT(
      KC_GRV,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,         KC_PLUS, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  
-     KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LCBR,         KC_RCBR, KC_LEFT, KC_UP,    KC_DOWN, KC_RGHT, KC_BSLS, KC_PIPE,
+     KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LCBR,         KC_RCBR, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, KC_BSLS, KC_PIPE,
      _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_BSPC,         ALFRED,  KC_COPY, KC_PASTE, _______, _______, _______, _______,
      _______, _______,  _______, _______, _______, KC_BSPC,                           ALFRED,  ADJUST,   KC_HOME, KC_PGUP, KC_PGDN, KC_END 
   ),
  
   [_RAISE] = LAYOUT(
-     KC_TILD, KC_1,     KC_2,    KC_3,     KC_PLUS, KC_MINS,  _______,        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
-     _______, KC_4,     KC_5,    KC_6,     KC_PAST, KC_PSLS,  _______,        _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+     KC_TILD, KC_1,     KC_2,    KC_3,     KC_PLUS, KC_MINS, _______,        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
+     _______, KC_4,     KC_5,    KC_6,     KC_PAST, KC_PSLS, _______,        _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
      _______, KC_7,     KC_8,    KC_9,     KC_COMM, KC_DOT,  ALFRED,         _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,
-     _______, _______,  KC_0,    _______,  _______, ALFRED,                            KC_BSPC, _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT 
+     _______, _______,  KC_0,    _______,  _______, ALFRED,                           KC_BSPC, _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT 
   ),
 
   [_ADJUST] = LAYOUT(
