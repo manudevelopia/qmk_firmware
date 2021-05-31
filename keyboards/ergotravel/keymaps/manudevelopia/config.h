21f7c447e5d5620534771b15d5253d382e929c1b
Manuel Garcia <Manuel.Garcia@zooplus.com>
2021-05-31 10:13:18 +0200

    increase auto-shift value

--- keyboards/ergotravel/keymaps/manudevelopia/config.h
+++ keyboards/ergotravel/keymaps/manudevelopia/config.h
@@ -35,6 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 
-#define AUTO_SHIFT_TIMEOUT 175
+#define AUTO_SHIFT_TIMEOUT 200
 #define NO_AUTO_SHIFT_ALPHA
 #define AUTO_SHIFT_REPEAT
